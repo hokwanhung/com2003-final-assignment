a6d7a88b8b4708528e4734230f0c7c0ca2a7ee87
khho <hokwanhung.2019@gmail.com>
2023-09-14 15:48:55 +0800

    feat: recreate part of the lost code

---
+++ define_df.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+
+def add_sma_crossover(df):
+    copy = df.copy()
+    # Calculates the and assign the moving average for the specified window size.
+    copy["SMA_50"] = copy["Close"].rolling(50).mean()
+    copy["SMA_200"] = copy["Close"].rolling(200).mean()
+    return copy
+
+
+def add_sma_crossover_with_signals(df):
+    copy = df.copy()
+    # Calculate the and assign the moving average for the specified window size.
+    copy["SMA_50"] = copy["Close"].rolling(50).mean()
+    copy["SMA_200"] = copy["Close"].rolling(200).mean()
+
+    # Generate signals with regards to bullish signals and bearish signals.
+    # SMA_50 > SMA_200 (crosses above) = bullish signals.
+    # SMA_50 < SMA_200 (crosses below) = bearish signals.
+    copy["Signal"] = 0
+    copy.loc[(copy["SMA_50"] > copy["SMA_200"]) & (copy["SMA_50"].shift() < copy["SMA_200"].shift()), "Signal"] = 1
+    copy.loc[(copy["SMA_50"] < copy["SMA_200"]) & (copy["SMA_50"].shift() > copy["SMA_200"].shift()), "Signal"] = -1
+
+    return copy
+
+
+def add_stochastic_oscillator(df, periods=14):
+    # 14, 3, 3:
+    # 14 specfies the number of periods used t0 calculate the %K line (i.e. fast line).
+    # 3 specifies the number of periods used to calculate the %D line (i.e. slow line).
+    # 3 specifies the smoothing factor used for the %D line.
+
+    # The longer the more reliable; the shorter the more fluctuate.
+    copy = df.copy()
+
+    high_roll = copy["High"].rolling(periods).max()
+    low_roll = copy["Low"].rolling(periods).min()
+    print(high_roll)
+    print(low_roll)
+
+    # Fast stochastic indicator
+    # %K = 100 * [(Close - Lowest Low in Period)/(Highest High in Period - Lowest Low in Period)]
+    # More volatile.
+    num = copy["Close"] - low_roll
+    denom = high_roll - low_roll
+    copy["%K"] = (num / denom) * 100
+
+    # Slow stochastic indicator
+    # Uses Simple Moving Average of the last 3 %K values using rolling() and mean() functions.
+    # Less sensitive to price changes and provides a smoother representation.
+    copy["%D"] = copy["%K"].rolling(3).mean()
+
+    return copy
+
+def add_stochastic_oscillator_strats_with_signals(df, periods=14, strat=0):
+    # 14, 3, 3:
+    # 14 specfies the number of periods used t0 calculate the %K line (i.e. fast line).
+    # 3 specifies the number of periods used to calculate the %D line (i.e. slow line).
+    # 3 specifies the smoothing factor used for the %D line.
+
+    # The longer the more reliable; the shorter the more fluctuate.
+    copy = df.copy()
+
+    high_roll = copy["High"].rolling(periods).max()
+    low_roll = copy["Low"].rolling(periods).min()
+    print(high_roll)
+    print(low_roll)
+
+    # Fast stochastic indicator
+    # %K = 100 * [(Close - Lowest Low in Period)/(Highest High in Period - Lowest Low in Period)]
+    # More volatile.
+    num = copy["Close"] - low_roll
+    denom = high_roll - low_roll
+    copy["%K"] = (num / denom) * 100
+
+    # Slow stochastic indicator
+    # Uses Simple Moving Average of the last 3 %K values using rolling() and mean() functions.
+    # Less sensitive to price changes and provides a smoother representation.
+    copy["%D"] = copy["%K"].rolling(3).mean()
+
+    copy["Signal"] = 0
+    if strat == 1:
+        # Generate signals with regards to overbought signals and oversold signals.
+        # %K > 80 = overbought signals.
+        # %K < 20 = oversold signals.
+        copy.loc[copy["%K"] < 20, "Signal"] = 1
+        copy.loc[copy["%K"] > 80, "Signal"] = -1
+    elif strat == 2:
+        # Generate signals with regards to overbought divergence signals.
+        # Price diff > 0 && %K Diff < 0 = bullish signals.
+        # Price diff < 0 && %K Diff > 0 = bearish signals.
+        copy.loc[(copy["Close"].diff() > 0) & (
+            copy["%K"].diff() < 0), "Signal"] = 1
+        copy.loc[(copy["Close"].diff() < 0) & (
+            copy["%K"].diff() > 0), "Signal"] = -1
+    elif strat == 3:
+        # Generate signals with regards to
+        copy.loc[(copy["%K"] > copy["%D"]), "Signal"] = 1
+        copy.loc[(copy["%K"] < copy["%D"]), "Signal"] = -1
+    else:
+        print("WARN: no strategy has been applied.")
+
+    return copy
---
+++ plot_df.py
@@ -0,0 +1,175 @@
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import mplfinance as mpf
+
+from matplotlib.lines import Line2D
+
+
+today = datetime.datetime.now()
+
+date_pattern = "%Y-%m-%d"
+## Formats today DateTime Object into the above date_pattern
+today_str = today.strftime(date_pattern)
+## Creates a 'date_ranges' dictionary and values corresponding to the start date of each interval relative to 'today'.
+## timedelta() function allows to perform arithmetic with dates and times.
+date_ranges = {
+    "1M": (today - datetime.timedelta(days=30)).strftime(date_pattern),
+    "3M": (today - datetime.timedelta(days=90)).strftime(date_pattern),
+    "6M": (today - datetime.timedelta(days=180)).strftime(date_pattern),
+    "1Y": (today - datetime.timedelta(days=365)).strftime(date_pattern),
+    "2Y": (today - datetime.timedelta(days=2*365)).strftime(date_pattern),
+}
+
+
+def plot_candlestick(df, symbol, rng):
+    start = date_ranges[rng]
+    end = today_str
+    ## Plots a candlestick chart using the mplfinance library.
+    mpf.plot(
+        df[start:end],
+        type="candle",
+        title=f"{symbol} Price, {rng}",
+        ylabel="Price ($)",
+        volume=True,
+        ylabel_lower="Volume",
+        show_nontrading=False,
+        mav=(4),
+        figsize=(8, 3),
+        style="yahoo",
+        datetime_format="%Y-%m-%d")
+
+
+def plot_sma_crossover(df, symbol, rng):
+    start = date_ranges[rng]
+    end = today_str
+    ## Selects and store the columns "Close", "SMA_50" and "SMA_200" from the DataFrame.
+    temp_df = df.loc[start:end, ["Close", "SMA_50", "SMA_200"]]
+    temp_df.plot(
+        title=f"{symbol} SMA Crossover, {rng}",
+        style=["-", "-", "--"],
+        figsize=(8, 3))
+
+
+# TODO Support plotting against index and also peers
+def plot_capital_appreciation(df, symbol, rng):
+    start = date_ranges[rng]
+    end = today_str
+    
+    # Combined frame with multiple tickers.
+    # Currently there's just one but we want to add index and peers as indicated above.
+    comb_df = pd.DataFrame({
+        symbol: df.loc[start:end, "Close"],
+    })
+    
+    norm_df = comb_df.div(comb_df.iloc[0])
+    norm_df.plot(title=f"{symbol} capital appreciation, {rng}", figsize=(8, 3))
+
+
+def plot_bollinger(df, symbol, rng):
+    start = date_ranges[rng]
+    end = today_str
+    temp_df = df.loc[start:end, ["Close", "SMA_20", "UpperBB", "LowerBB"]]
+    temp_df.plot(
+        title=f"{symbol} with Bollinger Bands, {rng}",
+        style=["-", "--", "-", "-"],
+        figsize=(8, 3))
+
+
+def plot_stochastic_oscillator(df, symbol, rng, periods=14):
+    start = date_ranges[rng]
+    end = today_str
+    temp_df = df[start:end]
+    
+    fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, tight_layout=True, figsize=(8, 6))
+
+    ax[0].set_title(f"{symbol} price, {rng}")
+    ax[0].plot(temp_df["Close"], color="tab:blue")
+
+    ax[1].set_title(f"{symbol} Stochastic Oscillator ({periods}-day period), {rng}")
+    ax[1].set_ylim(-10, 110)
+    ax[1].plot(temp_df["%K"], color="tab:blue") # fast
+    ax[1].plot(temp_df["%D"], color="tab:orange") # slow
+    ax[1].axhline(80, color="tab:red", ls="--")
+    ax[1].axhline(20, color="tab:green", ls="--")
+
+    custom_lines = [
+        Line2D([0], [0], color="tab:blue", lw=4),
+        Line2D([0], [0], color="tab:orange", lw=4),
+        Line2D([0], [0], color="tab:red", lw=4),
+        Line2D([0], [0], color="tab:green", lw=4),
+    ]
+    ax[1].legend(custom_lines, ["%K", "%D", "Overbought", "Oversold"], loc="best")
+
+
+def plot_rsi(df, symbol, rng, periods=14):
+    start = date_ranges[rng]
+    end = today_str
+    temp_df = df[start:end]
+    
+    fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, tight_layout=True, figsize=(8, 6))
+
+    ax[0].set_title(f"{symbol} price, {rng}")
+    ax[0].plot(temp_df["Close"])
+
+    ax[1].set_title(f"{symbol} RSI ({periods}-day moving average), {rng}")
+    ax[1].set_ylim(0, 100)
+    ax[1].plot(temp_df["RSI"])
+    ax[1].axhline(70, color="tab:red", ls="--")
+    ax[1].axhline(30, color="tab:green", ls="--")
+
+    custom_lines = [
+        Line2D([0], [0], color="tab:red", lw=4),
+        Line2D([0], [0], color="tab:green", lw=4)
+    ]
+    ax[1].legend(custom_lines, ["Overbought", "Oversold"], loc="best")
+
+
+def plot_macd(df, symbol, rng, periods=14):
+    start = date_ranges[rng]
+    end = today_str
+    temp_df = df[start:end]
+    
+    fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, tight_layout=True, figsize=(8, 6))
+
+    ax[0].set_title(f"{symbol} price, {rng}")
+    ax[0].plot(temp_df["Close"])
+
+    ax[1].set_title(f"{symbol} MACD, {rng}")
+    ax[1].plot(temp_df["MACD"], color="tab:blue") # slow signal
+    ax[1].plot(temp_df["MACD-s"], color="tab:orange") # fast signal
+    ax[1].bar(temp_df.index, height=temp_df["MACD-h"], color="black") # diff
+
+    custom_lines = [
+        Line2D([0], [0], color="tab:blue", lw=4),
+        Line2D([0], [0], color="tab:orange", lw=4),
+        Line2D([0], [0], color="black", lw=4)
+    ]
+    ax[1].legend(custom_lines, ["MACD", "Signal", "Diff"], loc="best")
+
+
+    start = date_ranges[rng]
+    end = today_str
+def plot_obv(df, symbol, rng):
+    temp_df = df[start:end]
+    
+    # See
+    # https://github.com/matplotlib/mplfinance/blob/master/examples/addplot.ipynb
+    # https://github.com/matplotlib/mplfinance/blob/8fa38f2dcd6d3b75c97145b5ded953f13641625e/src/mplfinance/plotting.py#L36
+    obv_addplot = mpf.make_addplot(temp_df["OBV"], width=2, panel=2, ylabel="OBV")
+    
+    # See https://github.com/DanielGoldfarb/mplfinance/blob/master/examples/panels.ipynb
+    mpf.plot(
+        temp_df,
+        addplot=obv_addplot,
+        type="candle",
+        title=f"{symbol} Price and On-Balance Volume, {rng}",
+        ylabel="Price ($)",
+        volume=True,
+        ylabel_lower="Volume",
+        show_nontrading=False,
+        style="yahoo",
+        figsize=(8, 4),
+        panel_ratios=(3, 1, 2),
+        datetime_format="%Y-%m-%d")
